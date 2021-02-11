ccf03cb8b40a6293617371c8244f9116e8faf5e6
Carlos Barros <carlos_dbs@hotmail.com>
2021-02-11 18:53:40 -0300

    Add DistanceBetweenBikesChallenge

---
+++ src/DistanceBetweenBikesChallenge/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DistanceBetweenBikesChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quilometros = 0;
+            if(Int32.TryParse(Console.ReadLine(), out int K))
+            {
+               quilometros = K;
+                int minutos = quilometros * 2; // Digite aqui o calculo dos minutos
+                Console.WriteLine(minutos + " minutos");
+            }
+            else
+            {
+                throw new ArgumentException("Digite um valor inteiro para os quilometros");
+            }
+            
+        }
+    }
+}
